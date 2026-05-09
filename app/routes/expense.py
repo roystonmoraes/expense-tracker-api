7cb93fb002c275a19666d4898c2f617f0e97125e
Royston Moraes <royston769@live.com>
2026-05-09 16:09:37 +0530

    CRUD operations

--- app/routes/expense.py
+++ app/routes/expense.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy.orm import Session
 
@@ -33,3 +33,81 @@ def create_expense(
     db.refresh(new_expense)
 
     return new_expense
+
+
+@router.get("/", response_model=list[ExpenseResponse])
+def get_expenses(
+    db: Session = Depends(get_db), current_user: User = Depends(get_current_user)
+):
+    expenses = db.query(Expense).filter(Expense.user_id == current_user.id).all()
+
+    return expenses
+
+
+@router.get("/{expense_id}", response_model=ExpenseResponse)
+def get_single_expense(
+    expense_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    expense = (
+        db.query(Expense)
+        .filter(Expense.id == expense_id, Expense.user_id == current_user.id)
+        .first()
+    )
+
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+
+    return expense
+
+
+@router.put("/{expense_id}", response_model=ExpenseResponse)
+def update_expense(
+    expense_id: int,
+    updated_expense: ExpenseCreate,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    expense = (
+        db.query(Expense)
+        .filter(Expense.id == expense_id, Expense.user_id == current_user.id)
+        .first()
+    )
+
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+
+    expense.title = updated_expense.title
+    expense.amount = updated_expense.amount
+    expense.category = updated_expense.category
+    expense.expense_date = updated_expense.expense_date
+
+    db.commit()
+
+    db.refresh(expense)
+
+    return expense
+
+
+@router.delete("/{expense_id}")
+def delete_expense(
+    expense_id: int,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+
+    expense = (
+        db.query(Expense)
+        .filter(Expense.id == expense_id, Expense.user_id == current_user.id)
+        .first()
+    )
+
+    if not expense:
+        raise HTTPException(status_code=404, detail="Expense not found")
+
+    db.delete(expense)
+
+    db.commit()
+
+    return {"message": "Expense deleted successfully"}
